5e8cbf3a2b98624b31597275bddf0cab13e1195b
lin Ngo <lin.r.ngo-1@ou.edu>
2019-10-22 11:57:52 -0500

    finish implemeting and testing DateTimeOne

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,12 +1,17 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
-	private Date now = new Date();
-	LocalDateTime locNow = LocalDateTime.now();
+	
 
+	private LocalDateTime locNow = LocalDateTime.now();
+	
 
 	@Override
 	int getValueOfSecond() {
@@ -55,8 +60,45 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 
 	@Override
 	void timeZoneHashMap() {
-		// TODO Auto-generated method stub
+		DateTimeFormatter time = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
+			LocalDateTime gmt = locNow.plusHours(5);
+			LocalDateTime bst = gmt.plusHours(6);
+			LocalDateTime cst = locNow;
 		
+		Map<String,String> style1 = new HashMap<String,String>();
+			style1.put("BST", time.format(bst));
+			style1.put("AST", "10/01/2020 19:59");
+			style1.put("CST", time.format(cst));
+			style1.put("ZST", "11/05/2018 19:59");
+			style1.put("GMT", time.format(gmt));
+		System.out.println("\nPrint Style 1: ");
+		 TreeMap<String, String> sort = new TreeMap<>(style1); 
+	        for (Map.Entry<String, String> entry : sort.entrySet())  
+	            System.out.println(entry.getKey() + " " + entry.getValue());  
+		
+	    Map<String,String> style2 = new HashMap<String,String>();
+			style2.put(style1.get("BST"),time.format(bst));
+			style2.put(style1.get("AST"),time.format(bst));
+			style2.put(style1.get("CST"),time.format(bst));
+			style2.put(style1.get("ZST"),time.format(bst));
+			style2.put(style1.get("GMT"),time.format(bst));
+		System.out.println("Print Style 3: ");
+		 sort = new TreeMap<>(style2); 
+	        for (Map.Entry<String, String> entry : sort.entrySet())  
+	            System.out.println(entry.getKey());
+	        
+	    LocalDateTime[] style3 = new LocalDateTime[5];
+		    style3[0] = LocalDateTime.parse(style1.get("BST"), time);
+		    style3[1] = LocalDateTime.parse(style1.get("AST"), time);
+		    style3[2] = LocalDateTime.parse(style1.get("CST"), time);
+		    style3[3] = LocalDateTime.parse(style1.get("ZST"), time);
+		    style3[4] = LocalDateTime.parse(style1.get("GMT"), time);
+	    Arrays.sort(style3, Collections.reverseOrder());
+	    System.out.println("Print Style 5: Final sorted Array");
+	    	for (LocalDateTime e : style3)
+	    		System.out.println(e);
+	    
+	    
 	}
-   
+	
 }
