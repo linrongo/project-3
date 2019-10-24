5e9484e1713e4a2a98c81762855590e237c95cb8
lin Ngo <lin.r.ngo-1@ou.edu>
2019-10-24 16:13:06 -0500

    add description to DateSorting

--- DateSortingUsingAlgorithm.java
+++ DateSortingUsingAlgorithm.java
@@ -13,19 +13,21 @@ public class DateSortingUsingAlgorithm {
 	 private HashMap<LocalDate, Integer> dates;
 	 
 	public DateSortingUsingAlgorithm() throws IOException {
+	// read file
 		BufferedReader file = new BufferedReader(new FileReader("SortingDates.txt"));
-			dates = new HashMap<LocalDate,Integer>();
-			
+			dates = new HashMap<LocalDate,Integer>();		
 			String input = file.readLine();
 			
+		// Create DateTime format so it works with
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 			LocalDate date;
 			final int LIMIT = 20;
-			
+		// go though all the lines in the file	
 			for (int i = 1; i <= LIMIT; ++i){
+			// Trim and delete all spaces
 				input = input.trim();
 				input = input.replace(" ", "");
-				
+			// turn it LocalDate and put it in the HashMap	
 				date = LocalDate.parse(input, formatter);	
 				dates.put(date,i);
 				
@@ -34,13 +36,17 @@ public class DateSortingUsingAlgorithm {
 			}
 		file.close();
 	 }
+	
 	public void dateHashMapSortedDescending() {
+	// sort using TreeMap and Collection to reverse the order
 		TreeMap<LocalDate,Integer> sort = new TreeMap<>(Collections.reverseOrder());
 		sort.putAll(dates);
+	// print out the contents
         for (Map.Entry<LocalDate,Integer> entry : sort.entrySet())  
             System.out.println(entry.getKey());  				
 	}
 	public void dateHashMapSorted() {
+	// sort using TreeMap ad print out the contents
 		TreeMap<LocalDate,Integer> sort = new TreeMap<>(dates); 
         for (Map.Entry<LocalDate,Integer> entry : sort.entrySet())  
             System.out.println(entry.getKey());  			
--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -4,19 +4,15 @@ import java.util.TreeMap;
 
 public class MesoLexicographical extends MesoSortedAbstract
 {
-	private Map<String,Integer> unSortedMap;
-	
 	public MesoLexicographical(HashMap<String, Integer> asciiVal) {
-		this.unSortedMap = asciiVal;
 		Map<String, Integer> sort = sortedMap(asciiVal);
 		 for (Map.Entry<String, Integer> entry : sort.entrySet())  
 	            System.out.println(entry.getKey()); 
 	}
 
 	@Override
-	protected Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted) {
-		Map<String, Integer> sort = new TreeMap<>(unsorted); 
-        
+	protected Map<String, Integer> sortedMap (HashMap<String, Integer> unsorted) {
+		Map<String, Integer> sort = new TreeMap<>(unsorted);       
         return sort;
 	}
 }
