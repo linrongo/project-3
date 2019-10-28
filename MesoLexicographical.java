a441adeb3f7d9bc23007114c2ec0a494a86fee3a
lin Ngo <lin.r.ngo-1@ou.edu>
2019-10-28 13:16:08 -0500

    Update MesoLexicographical.java

--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -5,6 +5,7 @@ import java.util.TreeMap;
 public class MesoLexicographical extends MesoSortedAbstract
 {
 	public MesoLexicographical(HashMap<String, Integer> asciiVal) {
+	// get sorted map and print out contents
 		Map<String, Integer> sort = sortedMap(asciiVal);
 		 for (Map.Entry<String, Integer> entry : sort.entrySet())  
 	            System.out.println(entry.getKey()); 
@@ -12,6 +13,7 @@ public class MesoLexicographical extends MesoSortedAbstract
 
 	@Override
 	protected Map<String, Integer> sortedMap (HashMap<String, Integer> unsorted) {
+	// sort using TreeMap
 		Map<String, Integer> sort = new TreeMap<>(unsorted);       
         return sort;
 	}
